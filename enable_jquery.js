c2cc4bc707efa499c478cf66e3d781c123839769
percentage <b98901159@ntu.edu.tw>
2014-08-02 04:49:14 -0700

    Update enable_jquery.js
    Change CDN provider to google

--- enable_jquery.js
+++ enable_jquery.js
@@ -1,3 +1,3 @@
 var a = document.createElement('script');
-a.setAttribute('src', '//code.jquery.com/jquery-1.11.0.min.js');
+a.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
 document.getElementsByTagName('head')[0].appendChild(a);
